3a784e98e145532bc3d2b3e96866cdc2cd362f9c
DarccMan <darcy3057@gmail.com>
2021-07-27 11:07:13 +1000

    Triangle Added SVG conversion

--- p/triangle/game.js
+++ p/triangle/game.js
@@ -17,15 +17,8 @@ doc.id("wrap").style.height = data.canvas.h + "px";
 canvas = cv.main;
 ctx = ct.main;
 
-var gameState = "start";
-
-function reset() {
-
-  gameState = "play";
-}
-
 function render() {
-  ctx.fillCanvas("#CCC");
+  ctx.fillCanvas("#DDD");
 
   w = parseFloat($("#w").val());
   h = (w / 2) * Math.tan(rad(60));
@@ -169,50 +162,95 @@ function render() {
     ctx.stroke();
   }
 
+  path = [
+    [
+      "M",
+      50 - d * Math.tan(rad(30)),
+      50 - h / 2 + d,
+    ], [
+      "L",
+      50 - w / 2 + d / Math.sin(rad(60)) / 2,
+      50 + h / 2 - d,
+    ], [
+      "C",
+      50 - w / 2 + d2 / Math.sin(rad(60)) / 2,
+      50 + h / 2 - d2,
+      50 - w / 2 + d2 / Math.sin(rad(60)),
+      50 + h / 2,
+      50 - w / 2 + d / Math.sin(rad(60)),
+      50 + h / 2,
+    ], [
+      "L",
+      50 + w / 2 - d / Math.sin(rad(60)),
+      50 + h / 2,
+    ], [
+      "C",
+      50 + w / 2 - d2 / Math.sin(rad(60)),
+      50 + h / 2,
+      50 + w / 2 - d2 / Math.sin(rad(60)) / 2,
+      50 + h / 2 - d2,
+      50 + w / 2 - d / Math.sin(rad(60)) / 2,
+      50 + h / 2 - d,
+    ], [
+      "L",
+      50 + d * Math.tan(rad(30)),
+      50 - h / 2 + d,
+    ], [
+      "C",
+      50 + d2 * Math.tan(rad(30)),
+      50 - h / 2 + d2,
+      50 - d2 * Math.tan(rad(30)),
+      50 - h / 2 + d2,
+      50 - d * Math.tan(rad(30)),
+      50 - h / 2 + d,
+    ]
+  ];
+
   ctx.strokeStyle = "#000";
   ctx.lineWidth = 4;
   ctx.beginPath();
-  ctx.moveTo(
-    perc(50 - d * Math.tan(rad(30))),
-    perc(50 - h / 2 + d),
-  );
-  ctx.lineTo(
-    perc(50 - w / 2 + d / Math.sin(rad(60)) / 2),
-    perc(50 + h / 2 - d),
-  );
-  ctx.bezierCurveTo(
-    perc(50 - w / 2 + d2 / Math.sin(rad(60)) / 2),
-    perc(50 + h / 2 - d2),
-    perc(50 - w / 2 + d2 / Math.sin(rad(60))),
-    perc(50 + h / 2),
-    perc(50 - w / 2 + d / Math.sin(rad(60))),
-    perc(50 + h / 2),
-  );
-  ctx.lineTo(
-    perc(50 + w / 2 - d / Math.sin(rad(60))),
-    perc(50 + h / 2),
-  );
-  ctx.bezierCurveTo(
-    perc(50 + w / 2 - d2 / Math.sin(rad(60))),
-    perc(50 + h / 2),
-    perc(50 + w / 2 - d2 / Math.sin(rad(60)) / 2),
-    perc(50 + h / 2 - d2),
-    perc(50 + w / 2 - d / Math.sin(rad(60)) / 2),
-    perc(50 + h / 2 - d),
-  );
-  ctx.lineTo(
-    perc(50 + d * Math.tan(rad(30))),
-    perc(50 - h / 2 + d),
-  );
-  ctx.bezierCurveTo(
-    perc(50 + d2 * Math.tan(rad(30))),
-    perc(50 - h / 2 + d2),
-    perc(50 - d2 * Math.tan(rad(30))),
-    perc(50 - h / 2 + d2),
-    perc(50 - d * Math.tan(rad(30))),
-    perc(50 - h / 2 + d),
-  );
+  temp = [];
+  for (i = 0; i < path.length; i++) {
+    switch (path[i][0]) {
+      case "M": {
+        ctx.moveTo(
+          perc(path[i][1]),
+          perc(path[i][2]),
+        );
+      }; break;
+      case "L": {
+        ctx.lineTo(
+          perc(path[i][1]),
+          perc(path[i][2]),
+        );
+      }; break;
+      case "C": {
+        ctx.bezierCurveTo(
+          perc(path[i][1]),
+          perc(path[i][2]),
+          perc(path[i][3]),
+          perc(path[i][4]),
+          perc(path[i][5]),
+          perc(path[i][6]),
+        );
+      }; break;
+    }
+
+    temp.push(path[i].map(j => {
+      if (isNaN(parseFloat(j))) {
+        return j;
+      }
+      return j * ($("#size").val() / 100);
+    }).join(" "));
+  }
   ctx.stroke();
+  path = temp.join(" ");
+  $("#svg").attr("width", $("#size").val());
+  $("#svg").attr("height", $("#size").val());
+  $("#rect").attr("width", $("#size").val());
+  $("#rect").attr("height", $("#size").val());
+  $("#path").attr("d", path);
+  $("#output").text(path);
 }
 
 function perc(num) {
@@ -221,20 +259,3 @@ function perc(num) {
 function rad(deg) {
   return deg * Math.PI / 180;
 }
-
-function main() {
-  update((Date.now() - then) / 1000);
-  render();
-  then = Date.now();
-  requestAnimationFrame(main);
-}
-function update(mod) {
-  /* var keysDown = F.getKeyCodes(controls); */
-  if (gameState == "play") {
-
-  }
-}
-
-var then = Date.now();
-reset();
-main();
